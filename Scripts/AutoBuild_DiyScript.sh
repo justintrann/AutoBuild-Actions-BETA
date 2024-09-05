2afc180e5877b7a027c0a78ec648e320befb3dad
Justin Tran <justin@justinSurface.lan>
2024-09-05 21:45:21 +0700

    Update newifi3 config

--- Scripts/AutoBuild_DiyScript.sh
+++ Scripts/AutoBuild_DiyScript.sh
@@ -12,19 +12,19 @@ Firmware_Diy_Core() {
 	# ${OP_BRANCH}			OpenWrt 源码分支
 	# ${CONFIG_FILE}		配置文件
 	
-	Author=AUTO
+	Author=Justin Tran
 	# 作者名称, AUTO: [自动识别]
 	
-	Author_URL=AUTO
+	Author_URL=justintrs.me
 	# 自定义作者网站或域名, AUTO: [自动识别]
 	
 	Default_Flag=AUTO
 	# 固件标签 (名称后缀), 适用不同配置文件, AUTO: [自动识别]
 	
-	Default_IP="192.168.1.1"
+	Default_IP="192.168.9.1"
 	# 固件 IP 地址
 	
-	Default_Title="Powered by AutoBuild-Actions"
+	Default_Title="Built by Justin Tran"
 	# 固件终端首页显示的额外信息
 	
 	Short_Fw_Date=true
